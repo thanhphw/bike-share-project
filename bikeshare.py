654551e973cfd9a2dc9e86b0b1de82da5d2c1b52
Thanh Phuong <thanhphuongb1py@gmail.com>
2024-08-25 23:33:31 +0700

    Update code 1

--- bikeshare.py
+++ bikeshare.py
@@ -8,6 +8,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
+
     Asks user to specify a city, month, and day to analyze.
 
     Returns:
@@ -39,6 +40,7 @@ def get_filters():
 
 def load_data(city, month, day):
     """
+    
     Loads data for the specified city and filters by month and day if applicable.
     Args:
         (str) city - name of the city to analyze
